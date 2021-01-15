b3e59810cbb430fb00c578c9ecb7798dfeb4fd5f
Bruno Trojahn <73199546+BrunoTrojahnBolzan@users.noreply.github.com>
2021-01-14 21:13:10 -0300

    Melhorada a estrutura da API || Improved the API's structure
    PT-BR:
    Adicionado suporte a aninhamento de janelas e modificada a forma de tratamento de eventos.
    EN:
    Added window nesting support and changed the event handling method.

--- GUIndaste.h
+++ GUIndaste.h
@@ -28,22 +28,23 @@ Descrição: Esta é uma biblioteca que oferece funções para a criação de pr
 #define JANELA_TEXTO 0 //Macro para janela de texto estático
 #define JANELA_BOTAO 1 //Macro para janela botão
 #define JANELA_ENTRADA 2 //Macro para janela de texto editável
-#define BOTAO_PRESSIONADO 0 //Macro para evento de botão pressionado
-#define ROLAGEM_ABAIXO_VERTICAL 1 //Macro para evento de rolagem abaixo vertical
-#define ROLAGEM_ACIMA_VERTICAL 2 //Macro para evento de rolagem acima vertical
-#define ROLAGEM_ARRASTADA_VERTICAL 3 //Macro para evento de rolagem arrastada vertical
-#define ROLAGEM_ABAIXO_HORIZONTAL 4 //Macro para evento de rolagem abaixo horizontal
-#define ROLAGEM_ACIMA_HORIZONTAL 5 //Macro para evento de rolagem acima horizontal
-#define ROLAGEM_ARRASTADA_HORIZONTAL 6 //Macro para evento de rolagem arrastada horizontal
+#define JANELA_GENERICA 3 //Macro para janela genérica
+#define BOTAO_PRESSIONADO 1 //Macro para evento de botão pressionado
+#define ROLAGEM_ABAIXO_VERTICAL 2 //Macro para evento de rolagem abaixo vertical
+#define ROLAGEM_ACIMA_VERTICAL 3 //Macro para evento de rolagem acima vertical
+#define ROLAGEM_ARRASTADA_VERTICAL 4 //Macro para evento de rolagem arrastada vertical
+#define ROLAGEM_ABAIXO_HORIZONTAL 5 //Macro para evento de rolagem abaixo horizontal
+#define ROLAGEM_ACIMA_HORIZONTAL 6 //Macro para evento de rolagem acima horizontal
+#define ROLAGEM_ARRASTADA_HORIZONTAL 7 //Macro para evento de rolagem arrastada horizontal
 
 
 
 
 ///Protótipos de funções
 int main_GUIndaste(); //Protótipo da função principal do usuário
-int iniciar_janela(wchar_t *titulo, int x, int y, int largura, int altura, unsigned int cor, void (*callback)(int evento, int parametro), int rolagem_vertical, int rolagem_horizontal); //Protótipo da função criadora de uma janela principal
+int iniciar_janela(wchar_t *titulo, int x, int y, int largura, int altura, unsigned int cor, int rolagem_vertical, int rolagem_horizontal); //Protótipo da função criadora de uma janela principal
 void sair(); //Protótipo da função para sair do programa
-JANELA criar_janela(int tipo, wchar_t *texto, int x, int y, int largura, int altura, int comando, unsigned int cor); //Protótipo da função que cria janelas
+JANELA criar_janela(int tipo, wchar_t *texto, int x, int y, int largura, int altura, int comando, unsigned int cor, JANELA pai); //Protótipo da função que cria janelas
 int obter_texto_janela(wchar_t *destino, JANELA janela); //Protótipo da função que obtém o texto de uma janela
 int obter_janela_x(JANELA janela); //Protótipo da função que retorna a posição horizontal de uma janela
 int obter_janela_y(JANELA janela); //Protótipo da função que retorna a posição vertical de uma janela
@@ -70,12 +71,10 @@ int main_biblioteca(){
 
 
 //Função para iniciar janela
-int iniciar_janela(wchar_t *titulo, int x, int y, int largura, int altura, unsigned int cor, void (*callback)(int evento, int parametro), int rolagem_vertical, int rolagem_horizontal){
-
-    callback_function = callback; //Guarda o ponteiro para a função gestora de eventos do usuário da biblioteca
+int iniciar_janela(wchar_t *titulo, int x, int y, int largura, int altura, unsigned int cor, int rolagem_vertical, int rolagem_horizontal){
 
     //Chama a função interna equivalente e retorna -1 caso ela falhe
-    if(_iniciar_janela(titulo, x, y, largura, altura, cor, callback, rolagem_vertical, rolagem_horizontal))
+    if(_iniciar_janela(titulo, x, y, largura, altura, cor, rolagem_vertical, rolagem_horizontal))
         return -1;
 
     return 0; //Retorna 0 se a janela for iniciada
@@ -85,10 +84,10 @@ int iniciar_janela(wchar_t *titulo, int x, int y, int largura, int altura, unsig
 
 
 //Função que cria janela
-JANELA criar_janela(int tipo, wchar_t *texto, int x, int y, int largura, int altura, int comando, unsigned int cor){
+JANELA criar_janela(int tipo, wchar_t *texto, int x, int y, int largura, int altura, int comando, unsigned int cor, JANELA pai){
 
     //Chama a função interna de criar janela e armazena o retorno
-    JANELA retorno_janela = _criar_janela(tipo, texto, x, y, largura, altura, comando, cor);
+    JANELA retorno_janela = _criar_janela(tipo, texto, x, y, largura, altura, comando, cor, pai);
 
     return retorno_janela; //Retorna a janela criada
 }
@@ -97,11 +96,10 @@ JANELA criar_janela(int tipo, wchar_t *texto, int x, int y, int largura, int alt
 
 
 //Função que lê os eventos
-int ler_evento(){
+EVENTO ler_evento(){
 
-    _ler_evento(); //Chama a função interna leitora de eventos
+    return _ler_evento(); //Chama a função interna leitora de eventos
 
-    return flag_sair; //Retorna de acordo com a flag
 }
 
 
@@ -162,6 +160,6 @@ void sair(){
 
     _sair(); //Chama a função interna de sair
 
-    flag_sair = 0; //Modifica a flag para término do programa
+    _evento.evento = 0; //Modifica a flag para término do programa
 
 }
